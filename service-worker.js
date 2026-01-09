be50a40877c133d02518105a73a147f8e62a2cf9
safal00 <41604885+safal00@users.noreply.github.com>
2026-01-09 07:14:15 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+﻿// Registering the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('kiosk-cache').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'assets/icon.png',
+        'assets/gmail.png',
+        'assets/commonapp.png',
+        'assets/yahoo.png',
+        'assets/collegeboard.png',
+        'assets/icloud.png',
+        // Add other assets as needed
+      ]);
+    })
+  );
+});
+
+// Fetching from the cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response or fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
